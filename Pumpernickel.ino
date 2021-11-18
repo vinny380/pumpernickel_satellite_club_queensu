93d7a1799c4c4f71a97eb2e567a4fa0e040dd067
TomasHarmer <87254703+TomasHarmer@users.noreply.github.com>
2021-11-17 19:47:01 -0500

    Update Pumpernickel.ino

--- Pumpernickel.ino
+++ Pumpernickel.ino
@@ -11,8 +11,24 @@
 
 File myFile;
 
-void setup() {
+//Temperature-------------------------------------------------
 
+int sensor_pin = 0;
+int analog_data;
+float temperatureC; 
+
+//Pressure----------------------------------------------------
+
+#include <SFE_BMP180.h>
+#include <Wire.h>
+#define ALTITUDE 1655.0 
+SFE_BMP180 pressure_sensor;
+
+
+  //________________________________________________________________________________________________________________________________________________________________
+void setup() {
+  //________________________________________________________________________________________________________________________________________________________________
+  
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
@@ -57,9 +73,44 @@ void setup() {
     // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
   }
+  
+  
+  
+   //Temperature-----------------------------------------------
+  // start up temperature sensor 
+  Serial.begin(9600);
+
+  //Pressure--------------------------------------------------
+  // start up the pressure sensor 
+  pressure_sensor.begin(); 
+  
+  
+ 
 }
 
+  //________________________________________________________________________________________________________________________________________________________________
 void loop() {
+  //________________________________________________________________________________________________________________________________________________________________
+  
+   // read the analog input
+  analog_data = analogRead(sensor_pin);
+  
+  // convert analog input to a temperature value
+  temperatureC = (analog_data - 0.5) * 100; 
+  
+  // print out our values 
+  Serial.print("Analog Value: ");
+  Serial.println(analog_data);
+  
+  Serial.print("Temperature: ");
+  Serial.print(temperatureC);
+  Serial.println(" C");
+  Serial.println("");
+
+// pressure
+  char status;
+    double T,P,p0,a;
+  
       //Temperature----------------------------
     
     // You must first get a temperature measurement to perform a pressure reading.
@@ -68,6 +119,23 @@ void loop() {
     
     // Retrieve the completed temperature measurement, measurement is stored in the variable T.
     status = pressure.getTemperature(T);
+  
+      //Pressure----------------------------
+    // Start a pressure measurement: the parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+    status = pressure.startPressure(3);
+    delay(status);
+    
+    // Retrieve the completed pressure measurement, measurement is stored in the variable P.
+    status = pressure.getPressure(P,T);
+    
+    // Retrieve the relative (sea-level) pressure, measurement is stored in the variable p0. 
+    p0 = pressure.sealevel(P, ALTITUDE);
+    
+    // Retreive the current altitude from the pressure reading
+    a = pressure.altitude(P,p0);
+    
+    // include a delay to not overwork the sensor 
+    --------------------------------------
   
 }
 //-------------------------------------------------------------------------------------
@@ -95,6 +163,8 @@ void setup()
   //Pressure--------------------------------------------------
   // start up the pressure sensor 
   pressure_sensor.begin(); 
+  
+  
 }
 //////////////////////////////////////////////////////////////////////////
 void loop()
